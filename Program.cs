5ee5f80705302054fdec35976d2d4904861ce3ca
Roman Filatov <ikardoc@gmail.com>
2022-04-24 02:22:03 +0300

    Delete old files and folders

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string path = @"c:\Users\Admin\Downloads\30da91a108\";
+
+DirectoryInfo di = new DirectoryInfo(path);
+
+foreach (FileInfo file in di.GetFiles().Where(x => (DateTime.Now - x.LastWriteTime).TotalMinutes > 30))
+{
+    file.Delete();
+}
+foreach (DirectoryInfo dir in di.GetDirectories().Where(x => (DateTime.Now - x.LastWriteTime).TotalMinutes > 30))
+{
+    dir.Delete(true);
+}
